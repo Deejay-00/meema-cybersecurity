6ca6d5be12a00ef932858ae910ca2364e471976b
Jamiu <jammy190@gmail.com>
2022-10-25 10:40:19 +0100

    added content

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+let index = 0;
+let images = document.querySelectorAll('.image');
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+
+function changeImg(){
+    image[index].classList.remove('active');
+    index = (index + 1) % image.length;
+    image[index].classList.add('active');
+    
+    setInterval('changeImg()', 2000);
+
+}
+
+window.onload = changeImg();
+
+
